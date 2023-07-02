b4a38ae7f0da141ed3389d3a7f14de97fc887b91
Mutindi Hope <msndindi@outlook.com>
2023-07-02 18:55:01 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -18,17 +18,26 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
+@app.before_request
+def check_if_logged_in():
+    access_list = [
+        'clear',
+        'article_list',
+        'show_article',
+        'login',
+        'logout',
+        'check_session'
+    ]
+    if (request.endpoint) not in access_list and (not session.get('user_id')): 
+        return {'error': 'Unauthorized'}, 401
 
-    def delete(self):
-    
+class ClearSession(Resource):
+    def delete(self):    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
-class IndexArticle(Resource):
-    
+class IndexArticle(Resource):    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return make_response(jsonify(articles), 200)
@@ -36,63 +45,47 @@ class IndexArticle(Resource):
 class ShowArticle(Resource):
 
     def get(self, id):
-
         article = Article.query.filter(Article.id == id).first()
         article_json = article.to_dict()
-
         if not session.get('user_id'):
             session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
             session['page_views'] += 1
-
             if session['page_views'] <= 3:
                 return article_json, 200
-
             return {'message': 'Maximum pageview limit reached'}, 401
-
         return article_json, 200
 
 class Login(Resource):
-
-    def post(self):
-        
+    def post(self):        
         username = request.get_json().get('username')
         user = User.query.filter(User.username == username).first()
-
-        if user:
-        
+        if user:        
             session['user_id'] = user.id
             return user.to_dict(), 200
-
         return {}, 401
 
 class Logout(Resource):
-
     def delete(self):
-
-        session['user_id'] = None
-        
+        session['user_id'] = None        
         return {}, 204
 
 class CheckSession(Resource):
-
-    def get(self):
-        
+    def get(self):        
         user_id = session['user_id']
         if user_id:
             user = User.query.filter(User.id == user_id).first()
-            return user.to_dict(), 200
-        
+            return user.to_dict(), 200        
         return {}, 401
 
-class MemberOnlyIndex(Resource):
-    
-    def get(self):
-        pass
+class MemberOnlyIndex(Resource):    
+    def get(self):    
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
 
-class MemberOnlyArticle(Resource):
-    
+class MemberOnlyArticle(Resource):    
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
